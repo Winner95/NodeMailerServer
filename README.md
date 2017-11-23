b9198f61bcefe21338a251832363dbed16eb81cd
GitKeeper <info@sfera-pr.ru>
2017-11-23 01:14:53 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # NodeMailerServer
 nodeMailer server, which is run on Heroku + any remote mail-service
+
+## How to start
+
+Simply follow these steps:
+
+1) Register at Heroku
+2) Upload this code to instance
+3) Add to Herku instance your env_variables from index.js
+4) Add any email service credential (without second factor) to env_vvariables
+5) Apply CORS to Heroku instance, if needed
+6) Run post requests to your Heroku instance with certain data:
+	- name
+	- email
+	- phone
+
+
+Feel free to use or modify this tpl
---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var path = require("path");
+var nodemailer = require('nodemailer');
+var port = process.env.PORT; // port for running node instance
+var user = process.env.EMAIL_USER; // user login, also sender name
+var pass = process.env.EMAIL_PASS; // user pass
+var emailHost = process.env.EMAIL_HOST; // email service url
+var emailPort = process.env.EMAIL_PORT; // email service port
+var ssl = process.env.EMAIL_SSL; // boolean for security
+var recepients = process.env.EMAIL_RECEPIENTS; // email for receiving
+var hiddenRecepients = process.env.EMAIL_HIDDEN_RECEPIENTS; // email for hidden receiving
+var subject = process.env.EMAIL_SUBJECT; // subject of letter
+
+var transporter = nodemailer.createTransport({
+    host: emailHost,
+    port: emailPort,
+    secure: ssl, 
+    auth: {
+        user: user,
+        pass: pass
+    }
+});
+
+transporter.verify(function(error, success) {
+    if (error) {
+         console.log(error);
+    } else {
+         console.log('Server is ready to take our messages');
+    }
+ });
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/sendEmail', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+
+    var emailContent = req.body;
+
+    var mailOptions = {
+        from: '<' + user + '>' + emailContent.name,
+        to: recepients,
+        bcc: hiddenRecepients,
+        subject: subject + emailContent.position,
+        text: 'Имя: ' + emailContent.name + '\n Мобильный: ' + emailContent.phone + ' \n e-mail: ' + emailContent.email,
+        html: '<b>Имя:</b> ' + emailContent.name +  '<br> <b>Мобильный:</b> ' + emailContent.phone +  '<br> <b>e-mail:</b> ' + emailContent.email
+    };
+
+    // console.log(JSON.stringify(mailOptions));
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if(error) {
+            res.json(error);
+        } else {
+            console.log('Message sent: ' + info.response);
+            res.json({yo: info.response});
+        }
+    });
+    
+    res.send(emailContent);
+});
+
+app.listen(port, function () {
+    console.log('Server is running. Point your browser to: http://localhost:' + port);
+});
